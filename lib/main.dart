8729a6cc591661f08479f82a762ebfb30b31f722
aqdoan <87262232+aq-doan@users.noreply.github.com>
2024-06-10 21:32:25 +1000

    Revert "Dev (#2)"
    This reverts commit 700c18faaf5e02e37724e6d987e71ee0a6495eee.

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'match_details.dart';
-import 'match_add.dart';  // Import the new MatchAdd page
-import 'match.dart';
+import 'movie_details.dart';
+import 'movie.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,14 +21,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //BEGIN: the old MyApp builder from last week
     return ChangeNotifierProvider(
-        create: (context) => MatchModel(),
+        create: (context) => MovieModel(),
         child: MaterialApp(
-            title: 'Cricket Scoring App',
+            title: 'Database Tutorial',
             theme: ThemeData(
               primarySwatch: Colors.blue,
             ),
-            home: const MyHomePage(title: 'Cricket Scoring App')));
+            home: const MyHomePage(title: 'Database Tutorial')));
+    //END: the old MyApp builder from last week
   }
 }
 
@@ -45,50 +46,59 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Consumer<MatchModel>(builder: buildScaffold);
+    return Consumer<MovieModel>(builder: buildScaffold);
   }
 
-  Scaffold buildScaffold(BuildContext context, MatchModel matchModel, _) {
+  Scaffold buildScaffold(BuildContext context, MovieModel movieModel, _) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        //backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => const MatchAdd())); // Navigate to MatchAdd page
+          showDialog(
+              context: context,
+              builder: (context) {
+                return const MovieDetails();
+              });
         },
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            if (matchModel.loading)
+            //YOUR UI HERE
+            if (movieModel.loading)
               const CircularProgressIndicator()
             else
               Expanded(
                 child: ListView.builder(
                   itemBuilder: (_, index) {
-                    var match = matchModel.items[index];
+                    var movie = movieModel.items[index];
+                    var image = movie.image;
                     return Dismissible(
-                      key: Key(match.id),
+                      key: Key(movie.id), // Unique key for this item
                       onDismissed: (direction) {
-                        matchModel.delete(match.id);
+                        // Call the delete function from your MovieModel
+                        movieModel.delete(movie.id);
                       },
                       child: ListTile(
-                        title: Text(match.team1Name),
-                        subtitle: Text("VS ${match.team2Name}"),
+                        title: Text(movie.title),
+                        subtitle:
+                            Text("${movie.year} - ${movie.duration} Minutes"),
+                        leading: image != null ? Image.network(image) : null,
                         onTap: () {
                           Navigator.push(context,
                               MaterialPageRoute(builder: (context) {
-                            return MatchDetails(id: match.id);
+                            return MovieDetails(id: movie.id);
                           }));
                         },
                       ),
                     );
                   },
-                  itemCount: matchModel.items.length,
+                  itemCount: movieModel.items.length,
                 ),
               )
           ],
@@ -98,6 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+//A little helper widget to avoid runtime errors -- we can't just display a Text() by itself if not inside a MaterialApp, so this workaround does the job
 class FullScreenText extends StatelessWidget {
   final String text;
 
